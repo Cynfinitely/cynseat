0623051123d148ae602485fcf673013b407e263b
Celal Yasin Nari <celalyasinnari@gmail.com>
2024-11-15 21:08:41 +0200

    improvements

--- pages/tickets.tsx
+++ pages/tickets.tsx
@@ -1,12 +1,17 @@
 // pages/tickets.tsx
 
 import React, { useEffect, useState } from "react";
-import { useRouter } from "next/router";
-import { logout } from "../API/auth";
 import { useTranslation } from "react-i18next";
 import CheckoutButton from "@/components/CheckoutButton";
 import { auth, db } from "../firebase/firebase";
-import { collection, query, where, getDocs } from "firebase/firestore";
+import {
+  collection,
+  query,
+  where,
+  getDocs,
+  doc,
+  getDoc,
+} from "firebase/firestore";
 
 type Ticket = {
   number: number;
@@ -19,6 +24,10 @@ const Tickets: React.FC = () => {
   const { t } = useTranslation();
   const [tickets, setTickets] = useState<Ticket[]>([]);
   const [loading, setLoading] = useState(true);
+  const [totalTicketsSold, setTotalTicketsSold] = useState<number>(0);
+
+  // Define the starting ticket number
+  const startingTicketNumber = 77;
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((user) => {
@@ -32,6 +41,18 @@ const Tickets: React.FC = () => {
           const ticketsSnap = await getDocs(ticketsQuery);
           const tickets = ticketsSnap.docs.map((doc) => doc.data() as Ticket);
           setTickets(tickets);
+
+          // Fetch total tickets sold
+          const ticketNumberDoc = await getDoc(
+            doc(db, "tickets", "nextNumber")
+          );
+          const currentTicketNumber = ticketNumberDoc.exists()
+            ? ticketNumberDoc.data()?.number ?? startingTicketNumber
+            : startingTicketNumber - 1;
+
+          // Adjust the total tickets sold calculation
+          const totalTickets = currentTicketNumber - startingTicketNumber;
+          setTotalTicketsSold(totalTickets);
           setLoading(false);
         };
 
@@ -48,9 +69,17 @@ const Tickets: React.FC = () => {
   return (
     <div className="bg-red-100 w-full min-h-screen">
       <div className="flex flex-col justify-start items-center w-full px-4 md:px-0">
-        <h1 className="text-2xl md:text-3xl pl-2 my-2 border-l-4  font-sans font-bold border-teal-400 ">
-          {t("tickets")}
-        </h1>
+        {/* Adjusted Flex Container */}
+        <div className="flex flex-col md:flex-row items-center w-full relative">
+          <h1 className="text-2xl md:text-3xl pl-2 my-2 border-l-4 font-sans font-bold border-teal-400 mx-auto">
+            {t("tickets")}
+          </h1>
+          {/* Display total tickets sold */}
+          <p className="md:absolute md:right-10 my-4 md:mt-4 text-lg font-semibold">
+            {t("totalTicketsSold")}: {totalTicketsSold}
+          </p>
+        </div>
+
         <div className="flex gap-2 mt-4">
           <CheckoutButton />
         </div>
@@ -65,6 +94,7 @@ const Tickets: React.FC = () => {
         <p>
           {t("language")}: <b>Türkçe</b>
         </p>
+
         <div className="w-full overflow-auto">
           {loading ? (
             <div className="w-16 h-16 border-t-4 border-b-4 border-blue-500 rounded-full animate-spin"></div>
@@ -72,16 +102,14 @@ const Tickets: React.FC = () => {
             tickets.map((ticket) => (
               <div
                 key={ticket.number}
-                className="flex items-center justify-center"
-              >
+                className="flex items-center justify-center">
                 <div className="w-full px-4">
                   <div className="bg-white shadow-lg rounded-lg my-6 grid grid-cols-1 md:grid-cols-[auto,1fr]">
                     <div className="bg-gray-100 px-5 py-2 flex items-center justify-center __col h-full">
                       <a
                         href={ticket.imageUrl}
                         download
-                        className="text-blue-600 font-medium hover:text-blue-800"
-                      >
+                        className="text-blue-600 font-medium hover:text-blue-800">
                         {t("download")}
                       </a>
                     </div>
